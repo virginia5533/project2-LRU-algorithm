812f3f152ad637a3f955313d34b2d5d95421ba84
virginia5533 <vlv5@students.uwf.edu>
2021-10-14 20:34:13 -0500

    Setup for LRU in function

--- main.cpp
+++ main.cpp
@@ -10,10 +10,14 @@ using namespace std;
 
 
 vector<int> readInRef(vector<int> vect);
+void LRU(int f1, int f2, int f3, vector<int> vect);
 
 int main() {
 
 	vector<int> refStrVect;
+	int frame1, frame2, frame3;
+
+
 
 	cout << "Please enter the reference string separated by spaces: ";
 	refStrVect = readInRef(refStrVect);
@@ -21,11 +25,17 @@ int main() {
 
 	cout << "The reference string is: ";
 	for (int i = 0; i < refStrVect.size(); i++) {
-		cout << refStrVect.at(i); << '\t';
+		cout << refStrVect.at(i) << " ";
 
 	}
 
+	cout << endl;
+	cout << "Please enter the frame numbers: ";
+	cin >> frame1 >> frame2 >> frame3;
 
+	cout << "The frame numbers are: " << frame1 << " " << frame2 << " " << frame3 << endl;
+
+	LRU(frame1, frame2, frame3, refStrVect);
 
 }
 
@@ -36,12 +46,47 @@ vector<int> readInRef(vector<int> vect) {
 		int index;
 		cin >> index;
 
-		vect.push_back(location);
+		vect.push_back(index);
 	} while (cin.peek() != '\n');
 
 	return vect;
 }
 
+void LRU(int f1, int f2, int f3, vector<int> vect){
+
+	int pageFaults = 0;
+
+	vector<int> frame = { -1, -1, -1};
+
+	cout << "Assignment of frames by LRU algorithm:" << endl;
+	cout << "Frames: " << f1 << '\t' << f2 << '\t'<< f3 << endl;
+	cout << "------------------------------------------------------" << endl;
+
+	for(int i = 0; i < vect.size(); i++) {
+
+		for(int i = 0; i < frame.size(); i++) {
+
+			if(frame.at(i) == -1) {
+				frame.at(i) = vect.at(i);
+				cout << '\t' << frame.at(0) << '\t' <<  frame.at(1) << '\t' <<  frame.at(2) << endl;
+
+			}
+
+			else {
+				//LRU
+
+			}
+
+		}
+			cout << '\t' << frame.at(0) << '\t' <<  frame.at(1) << '\t' <<  frame.at(2) << endl;
+
+		
+
+
+
+	}
+
+}
 
 
 
